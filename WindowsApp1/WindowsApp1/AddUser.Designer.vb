ec5b0533ced33e440347aad39b9bbdc6d6a9b80d
Nakao <nakao.bks@gmail.com>
2021-04-06 17:59:50 +0900

    ユーザー登録画面作成

--- WindowsApp1/WindowsApp1/AddUser.Designer.vb
+++ WindowsApp1/WindowsApp1/AddUser.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class AddUser
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,88 @@ Partial Class AddUser
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.user_Id = New System.Windows.Forms.TextBox()
+        Me.pw = New System.Windows.Forms.TextBox()
+        Me.userId = New System.Windows.Forms.Label()
+        Me.password = New System.Windows.Forms.Label()
+        Me.AddUserButton = New System.Windows.Forms.Button()
+        Me.ReturnButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'user_Id
+        '
+        Me.user_Id.Location = New System.Drawing.Point(298, 116)
+        Me.user_Id.Name = "user_Id"
+        Me.user_Id.Size = New System.Drawing.Size(153, 22)
+        Me.user_Id.TabIndex = 0
+        '
+        'pw
+        '
+        Me.pw.Location = New System.Drawing.Point(298, 177)
+        Me.pw.Name = "pw"
+        Me.pw.Size = New System.Drawing.Size(153, 22)
+        Me.pw.TabIndex = 1
+        '
+        'userId
+        '
+        Me.userId.AutoSize = True
+        Me.userId.Location = New System.Drawing.Point(178, 119)
+        Me.userId.Name = "userId"
+        Me.userId.Size = New System.Drawing.Size(69, 15)
+        Me.userId.TabIndex = 2
+        Me.userId.Text = "ユーザーID"
+        '
+        'password
+        '
+        Me.password.AutoSize = True
+        Me.password.Location = New System.Drawing.Point(178, 180)
+        Me.password.Name = "password"
+        Me.password.Size = New System.Drawing.Size(67, 15)
+        Me.password.TabIndex = 3
+        Me.password.Text = "Password"
+        '
+        'AddUserButton
+        '
+        Me.AddUserButton.Location = New System.Drawing.Point(521, 341)
+        Me.AddUserButton.Name = "AddUserButton"
+        Me.AddUserButton.Size = New System.Drawing.Size(83, 41)
+        Me.AddUserButton.TabIndex = 4
+        Me.AddUserButton.Text = "登録"
+        Me.AddUserButton.UseVisualStyleBackColor = True
+        '
+        'ReturnButton
+        '
+        Me.ReturnButton.Location = New System.Drawing.Point(655, 341)
+        Me.ReturnButton.Name = "ReturnButton"
+        Me.ReturnButton.Size = New System.Drawing.Size(79, 41)
+        Me.ReturnButton.TabIndex = 5
+        Me.ReturnButton.Text = "戻る"
+        Me.ReturnButton.UseVisualStyleBackColor = True
+        '
+        'AddUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(828, 450)
+        Me.Controls.Add(Me.ReturnButton)
+        Me.Controls.Add(Me.AddUserButton)
+        Me.Controls.Add(Me.password)
+        Me.Controls.Add(Me.userId)
+        Me.Controls.Add(Me.pw)
+        Me.Controls.Add(Me.user_Id)
+        Me.Name = "AddUser"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents user_Id As TextBox
+    Friend WithEvents pw As TextBox
+    Friend WithEvents userId As Label
+    Friend WithEvents password As Label
+    Friend WithEvents AddUserButton As Button
+    Friend WithEvents ReturnButton As Button
 End Class
--- WindowsApp1/WindowsApp1/AddUser.vb
+++ WindowsApp1/WindowsApp1/AddUser.vb
@@ -1,3 +1,34 @@
-﻿Public Class AddUser
+﻿Imports MySql.Data.MySqlClient
 
+Public Class AddUser
+
+    Private Sub AddUserButton_Click(sender As Object, e As EventArgs) Handles AddUserButton.Click
+
+        '//user_Idを変数に代入
+        Dim userId As String = user_Id.Text
+
+        '//pwを変数に代入
+        Dim password As String = pw.Text
+
+        Dim sLogin As String = "server=localhost; database=BKSScheduledb; userid=BKSSCHEDULE; password=bksscd;"
+
+        Dim Conn As New MySqlConnection(sLogin)
+
+        Dim sql As String = "insert into user values ('" + userId + "','" + password + "')"
+
+        Dim adapter = New MySqlDataAdapter(sql, Conn)
+        Dim dt As New DataTable
+
+        Try
+
+            Conn.Open()
+            adapter.Fill(dt)
+
+        Catch mse As MySqlException
+            MessageBox.Show("Error:" + mse.Message)
+        Finally
+            Conn.Close()
+        End Try
+
+    End Sub
 End Class
