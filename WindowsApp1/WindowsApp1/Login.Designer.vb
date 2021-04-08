4f2d0131d6872f3d2e145d1aa7f55ce9f112a9a0
YoshidaRyousuke <r_yoshida@bs-ja.co.jp>
2021-04-08 11:38:39 +0900

    ログイン画面作成

--- WindowsApp1/WindowsApp1/Login.Designer.vb
+++ WindowsApp1/WindowsApp1/Login.Designer.vb
@@ -22,29 +22,120 @@ Partial Class Login
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.chkSaveLogin = New System.Windows.Forms.CheckBox()
+        Me.lnkAddUser = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtUserId = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'chkSaveLogin
         '
-        Me.Button1.Location = New System.Drawing.Point(226, 121)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.chkSaveLogin.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.chkSaveLogin.AutoSize = True
+        Me.chkSaveLogin.Location = New System.Drawing.Point(404, 174)
+        Me.chkSaveLogin.Name = "chkSaveLogin"
+        Me.chkSaveLogin.Size = New System.Drawing.Size(169, 19)
+        Me.chkSaveLogin.TabIndex = 4
+        Me.chkSaveLogin.Text = "ログイン情報を保存する"
+        Me.chkSaveLogin.UseVisualStyleBackColor = True
+        '
+        'lnkAddUser
+        '
+        Me.lnkAddUser.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lnkAddUser.AutoSize = True
+        Me.lnkAddUser.Location = New System.Drawing.Point(364, 257)
+        Me.lnkAddUser.Name = "lnkAddUser"
+        Me.lnkAddUser.Size = New System.Drawing.Size(38, 15)
+        Me.lnkAddUser.TabIndex = 7
+        Me.lnkAddUser.TabStop = True
+        Me.lnkAddUser.Text = "こちら"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(227, 257)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(131, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "初回ログインのかたは"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnLogin.Location = New System.Drawing.Point(112, 199)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(382, 37)
+        Me.btnLogin.TabIndex = 5
+        Me.btnLogin.Text = "ログイン"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 140)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ユーザID"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtPassword.Location = New System.Drawing.Point(112, 137)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(382, 22)
+        Me.txtPassword.TabIndex = 3
+        '
+        'txtUserId
+        '
+        Me.txtUserId.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtUserId.Location = New System.Drawing.Point(112, 66)
+        Me.txtUserId.Name = "txtUserId"
+        Me.txtUserId.Size = New System.Drawing.Size(382, 22)
+        Me.txtUserId.TabIndex = 1
         '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(627, 331)
+        Me.Controls.Add(Me.chkSaveLogin)
+        Me.Controls.Add(Me.lnkAddUser)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserId)
         Me.Name = "Login"
-        Me.Text = "Form2"
+        Me.Text = "ログイン画面"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents chkSaveLogin As CheckBox
+    Friend WithEvents lnkAddUser As LinkLabel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtUserId As TextBox
 End Class
--- WindowsApp1/WindowsApp1/Login.vb
+++ WindowsApp1/WindowsApp1/Login.vb
@@ -1,5 +1,84 @@
-﻿Public Class Login
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Login
+    ' DB接続情報のコンストラクタ
+    Const LOGIN As String = "server=localhost; database=BKSScheduledb; userid=BKSSCHEDULE; password=bksscd;"
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim Conn As New MySqlConnection(LOGIN)
+        ' 実行するSQL文を生成
+        Dim sql As String = "SELECT * FROM user WHERE USER_ID = '" + txtUserId.Text + "' AND PASSWORD = '" + txtPassword.Text + "';"
+        Dim adapter = New MySqlDataAdapter(sql, Conn)
+        ' データテーブルを作成
+        Dim dt As New DataTable
+        Dim errorMsg As String = ""
+        Try
+            ' DBと接続する
+            Conn.Open()
+            ' SQL文の実行結果をデータテーブルに格納する
+            adapter.Fill(dt)
+            ' ユーザID未入力チェック
+            If String.IsNullOrEmpty(txtUserId.Text) Then
+                errorMsg += "ユーザID" + Environment.NewLine
+            End If
+            ' パスワード未入力チェック
+            If String.IsNullOrEmpty(txtPassword.Text) Then
+                errorMsg += "Password" + Environment.NewLine
+            End If
+            ' エラーが発生していない場合
+            If String.IsNullOrEmpty(errorMsg) Then
+                ' ログイン成功
+                If dt.Rows.Count = 1 Then
+                    MessageBox.Show("ログイン成功", "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    ' ログイン情報を保存する
+                    SaveLogin()
+                    Dim addSchedule As AddSchedule = New AddSchedule()
+                    ' スケジュール登録画面に遷移する
+                    addSchedule.Show()
+                Else
+                    MessageBox.Show("そのユーザは登録されていません", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            Else
+                ' 未入力エラーメッセージを表示
+                MessageBox.Show(errorMsg + "が未入力です", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch mse As MySqlException
+            MessageBox.Show("Error:" + mse.Message)
+        Finally
+            ' DBとの接続をcloseする
+            Conn.Close()
+        End Try
+    End Sub
+
+    Private Sub lnkAddUser_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkAddUser.LinkClicked
+        ' 初回ログイン者用のユーザ登録ページに遷移させる
+        Dim addUser As AddUser = New AddUser()
+        addUser.Show()
+    End Sub
+
+    '''<summary>
+    '''ログイン情報を保存する
+    '''</summary>
+    Private Sub SaveLogin()
+        If chkSaveLogin.Checked Then
+            ' チェックが入っていれば入力されたログイン情報を保存する
+            My.Settings.userId = txtUserId.Text
+            My.Settings.password = txtPassword.Text
+        Else
+            ' チェックが入っていなければ""を保存する
+            My.Settings.userId = ""
+            My.Settings.password = ""
+        End If
+    End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' フォーム読み込み時、保存されたユーザID・パスワードを設定する
+        txtUserId.Text = My.Settings.userId
+        txtPassword.Text = My.Settings.password
+    End Sub
+
+    Private Sub txtPassword_TextChanged(sender As Object, e As EventArgs) Handles txtPassword.TextChanged
+        ' パスワードをアスタリスク表示にする
+        txtPassword.PasswordChar = "*"
     End Sub
 End Class
