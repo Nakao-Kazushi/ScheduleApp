d56036e2371ec3df54c99094592606f5cfdcdc73
浩延 浅川 <asakawa1115@yahoo.co.jp>
2021-04-09 10:18:31 +0900

    20210409朝

---
+++ windowsapp1/WindowsApp1/AddSchedule.vb
@@ -0,0 +1,55 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class AddSchedule
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim selected_startdate = Format(DateTimePicker1.Value, "yyyy/MM/dd")
+
+        Dim selected_enddate = Format(DateTimePicker2.Value, "yyyy/MM/dd")
+
+        Dim selected_starttime = NumericUpDown1.Value.ToString() + ":" + NumericUpDown2.Value.ToString()
+
+        Dim selected_endtime = NumericUpDown3.Value.ToString() + ":" + NumericUpDown4.Value.ToString()
+
+        Dim ins_id = Double.Parse(Format(Now(), "hhmmssfff")).ToString()
+
+        Dim Builder = New MySqlConnectionStringBuilder()
+        ' データベースに接続するために必要な情報をBuilderに与える
+        Builder.Server = "localhost"
+        Builder.Port = 3306
+        Builder.UserID = "BKSSCHEDULE"
+        Builder.Password = "bksscd"
+        Builder.Database = "BKSSCHEDULEdb"
+        Dim ConStr = Builder.ToString()
+        Dim Con As New MySqlConnection
+        Con.ConnectionString = ConStr
+        Con.Open()
+
+        Dim SqlStr = "SELECT * FROM Schedule"
+        Dim SqlStr2 = "insert into Schedule values('" + ins_id + "','" + selected_endtime + "','" + selected_startdate + "','" + selected_starttime + "','" + selected_endtime + "','" + selected_enddate + "')"
+
+        Dim adapter2 = New MySqlDataAdapter(SqlStr2, Con)
+        Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
+
+
+        Dim Ds As New DataSet
+        Dim dt As New DataTable
+        adapter2.Fill(dt)
+        Adapter.Fill(Ds)
+
+        DataGridView1.DataSource = Ds.Tables(0)
+        Con.Close()
+
+    End Sub
+
+    Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs) Handles MonthCalendar1.DateChanged
+        Dim selected_startdate = MonthCalendar1.SelectionRange.Start.ToString()
+        Dim selected_enddate = MonthCalendar1.SelectionRange.End.ToString()
+        DateTimePicker1.Value = selected_startdate
+        DateTimePicker2.Value = selected_enddate
+
+
+
+    End Sub
+End Class
