64e7eafb05d11f43be41d992ece853c793e2825f
浩延 浅川 <asakawa1115@yahoo.co.jp>
2021-04-09 18:16:03 +0900

    20210409終業前

--- WindowsApp1/WindowsApp1/AddSchedule.Designer.vb
+++ WindowsApp1/WindowsApp1/AddSchedule.Designer.vb
@@ -33,6 +33,9 @@ Partial Class AddSchedule
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Column1 = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewButtonColumn()
         CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -42,24 +45,25 @@ Partial Class AddSchedule
         '
         'MonthCalendar1
         '
-        Me.MonthCalendar1.Location = New System.Drawing.Point(87, 30)
+        Me.MonthCalendar1.Location = New System.Drawing.Point(85, 50)
         Me.MonthCalendar1.Name = "MonthCalendar1"
         Me.MonthCalendar1.ShowToday = False
         Me.MonthCalendar1.TabIndex = 0
         Me.MonthCalendar1.TodayDate = New Date(2021, 4, 7, 0, 0, 0, 0)
+        Me.MonthCalendar1.TrailingForeColor = System.Drawing.SystemColors.InactiveCaption
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(455, 181)
+        Me.Button1.Location = New System.Drawing.Point(607, 268)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(75, 26)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "登録"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'NumericUpDown1
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(368, 120)
+        Me.NumericUpDown1.Location = New System.Drawing.Point(402, 120)
         Me.NumericUpDown1.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
         Me.NumericUpDown1.Name = "NumericUpDown1"
         Me.NumericUpDown1.Size = New System.Drawing.Size(47, 22)
@@ -68,7 +72,7 @@ Partial Class AddSchedule
         'NumericUpDown2
         '
         Me.NumericUpDown2.Increment = New Decimal(New Integer() {5, 0, 0, 0})
-        Me.NumericUpDown2.Location = New System.Drawing.Point(422, 120)
+        Me.NumericUpDown2.Location = New System.Drawing.Point(451, 120)
         Me.NumericUpDown2.Maximum = New Decimal(New Integer() {60, 0, 0, 0})
         Me.NumericUpDown2.Name = "NumericUpDown2"
         Me.NumericUpDown2.Size = New System.Drawing.Size(46, 22)
@@ -77,7 +81,7 @@ Partial Class AddSchedule
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(484, 125)
+        Me.Label1.Location = New System.Drawing.Point(515, 125)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(22, 15)
         Me.Label1.TabIndex = 5
@@ -85,7 +89,7 @@ Partial Class AddSchedule
         '
         'NumericUpDown3
         '
-        Me.NumericUpDown3.Location = New System.Drawing.Point(522, 120)
+        Me.NumericUpDown3.Location = New System.Drawing.Point(558, 120)
         Me.NumericUpDown3.Maximum = New Decimal(New Integer() {24, 0, 0, 0})
         Me.NumericUpDown3.Name = "NumericUpDown3"
         Me.NumericUpDown3.Size = New System.Drawing.Size(47, 22)
@@ -94,7 +98,7 @@ Partial Class AddSchedule
         'NumericUpDown4
         '
         Me.NumericUpDown4.Increment = New Decimal(New Integer() {5, 0, 0, 0})
-        Me.NumericUpDown4.Location = New System.Drawing.Point(574, 120)
+        Me.NumericUpDown4.Location = New System.Drawing.Point(607, 120)
         Me.NumericUpDown4.Maximum = New Decimal(New Integer() {60, 0, 0, 0})
         Me.NumericUpDown4.Name = "NumericUpDown4"
         Me.NumericUpDown4.Size = New System.Drawing.Size(47, 22)
@@ -102,44 +106,79 @@ Partial Class AddSchedule
         '
         'DateTimePicker1
         '
-        Me.DateTimePicker1.Location = New System.Drawing.Point(347, 73)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(375, 73)
         Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(130, 22)
+        Me.DateTimePicker1.Size = New System.Drawing.Size(133, 22)
         Me.DateTimePicker1.TabIndex = 8
         Me.DateTimePicker1.Value = New Date(2021, 4, 8, 0, 0, 0, 0)
         '
         'DataGridView1
         '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(87, 261)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.DataGridView1.Location = New System.Drawing.Point(59, 309)
+        Me.DataGridView1.MultiSelect = False
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 4
         Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(645, 177)
+        Me.DataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(656, 357)
         Me.DataGridView1.TabIndex = 9
+        Me.DataGridView1.VirtualMode = True
         '
         'DateTimePicker2
         '
-        Me.DateTimePicker2.Location = New System.Drawing.Point(511, 73)
+        Me.DateTimePicker2.Location = New System.Drawing.Point(539, 73)
         Me.DateTimePicker2.Name = "DateTimePicker2"
-        Me.DateTimePicker2.Size = New System.Drawing.Size(130, 22)
+        Me.DateTimePicker2.Size = New System.Drawing.Size(143, 22)
         Me.DateTimePicker2.TabIndex = 10
         Me.DateTimePicker2.Value = New Date(2021, 4, 8, 0, 0, 0, 0)
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(483, 77)
+        Me.Label2.Location = New System.Drawing.Point(514, 77)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(22, 15)
         Me.Label2.TabIndex = 11
         Me.Label2.Text = "～"
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(375, 165)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(307, 89)
+        Me.TextBox1.TabIndex = 12
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = ""
+        Me.Column1.MinimumWidth = 6
+        Me.Column1.Name = "Column1"
+        Me.Column1.Text = "編集"
+        Me.Column1.UseColumnTextForButtonValue = True
+        Me.Column1.Width = 45
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = ""
+        Me.Column2.MinimumWidth = 6
+        Me.Column2.Name = "Column2"
+        Me.Column2.Text = "削除"
+        Me.Column2.UseColumnTextForButtonValue = True
+        Me.Column2.Width = 50
+        '
         'AddSchedule
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(818, 690)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.DateTimePicker2)
         Me.Controls.Add(Me.DataGridView1)
@@ -174,4 +213,7 @@ Partial Class AddSchedule
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents DateTimePicker2 As DateTimePicker
     Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Column1 As DataGridViewButtonColumn
+    Friend WithEvents Column2 As DataGridViewButtonColumn
 End Class
--- WindowsApp1/WindowsApp1/AddSchedule.vb
+++ WindowsApp1/WindowsApp1/AddSchedule.vb
@@ -4,11 +4,13 @@ Public Class AddSchedule
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim selected_date = Format(DateTimePicker1.Value, "yyyy/MM/dd")
+        Dim selected_startdate = Format(DateTimePicker1.Value, "yyyy/MM/dd")
 
-        Dim selected_time1 = NumericUpDown1.Value.ToString() + ":" + NumericUpDown2.Value.ToString()
+        Dim selected_enddate = Format(DateTimePicker2.Value, "yyyy/MM/dd")
 
-        Dim selected_time2 = NumericUpDown3.Value.ToString() + ":" + NumericUpDown4.Value.ToString()
+        Dim selected_starttime = NumericUpDown1.Value.ToString() + ":" + NumericUpDown2.Value.ToString()
+
+        Dim selected_endtime = NumericUpDown3.Value.ToString() + ":" + NumericUpDown4.Value.ToString()
 
         Dim ins_id = Double.Parse(Format(Now(), "hhmmssfff")).ToString()
 
@@ -24,11 +26,12 @@ Public Class AddSchedule
         Con.ConnectionString = ConStr
         Con.Open()
 
-        Dim SqlStr = "SELECT * FROM Schedule"
-        Dim SqlStr2 = "insert into Schedule values('" + ins_id + "','" + selected_time2 + "','" + selected_date + "','" + selected_time1 + "','" + selected_time2 + "')"
+        Dim SqlStr = "select regist_startdate as 開始日,regist_enddate as 終了日,regist_starttime as 開始時間,regist_endtime as 終了時間 from Schedule"
+        Dim SqlStr2 = "insert into Schedule values('" + ins_id + "','" + selected_endtime + "','" + selected_startdate + "','" + selected_starttime + "','" + selected_endtime + "','" + selected_enddate + "')"
 
         Dim adapter2 = New MySqlDataAdapter(SqlStr2, Con)
         Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
+
 
         Dim Ds As New DataSet
         Dim dt As New DataTable
@@ -45,7 +48,49 @@ Public Class AddSchedule
         Dim selected_enddate = MonthCalendar1.SelectionRange.End.ToString()
         DateTimePicker1.Value = selected_startdate
         DateTimePicker2.Value = selected_enddate
+        Dim Builder = New MySqlConnectionStringBuilder()
+        ' データベースに接続するために必要な情報をBuilderに与える
+        Builder.Server = "localhost"
+        Builder.Port = 3306
+        Builder.UserID = "BKSSCHEDULE"
+        Builder.Password = "bksscd"
+        Builder.Database = "BKSSCHEDULEdb"
+        Dim ConStr = Builder.ToString()
+        Dim Con As New MySqlConnection
+        Con.ConnectionString = ConStr
+        Con.Open()
+        Dim SqlStr = "select regist_startdate as 開始日, regist_enddate As 終了日, regist_starttime As 開始時間, regist_endtime As 終了時間 from Schedule
+        where regist_startdate between '" + selected_startdate + "' and '" + selected_enddate + "'"
+        Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
+        Dim Ds As New DataSet
+        Adapter.Fill(Ds)
+        DataGridView1.DataSource = Ds.Tables(0)
+        Con.Close()
 
+
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Dim OrderNo As String, OrderDate As String, DueDate As String, ProductNo As String, selectedrowno As Integer
+
+        OrderNo = DataGridView1.CurrentRow.Cells(2).Value.ToString()
+
+        OrderDate = DataGridView1.CurrentRow.Cells(3).Value.ToString()
+
+        DueDate = DataGridView1.CurrentRow.Cells(4).Value.ToString()
+
+        ProductNo = DataGridView1.CurrentRow.Cells(5).Value.ToString()
+
+        selectedrowno = DataGridView1.CurrentRow.Index
+
+        Dim dgv As DataGridView = CType(sender, DataGridView)
+
+        If dgv.Columns(e.ColumnIndex).Name = "Column1" Then
+
+            MessageBox.Show(selectedrowno)
+
+        End If
 
 
     End Sub
