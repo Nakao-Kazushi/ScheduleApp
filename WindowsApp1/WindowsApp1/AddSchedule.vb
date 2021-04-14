1fcbb4048ff71bed62e150a84965f0768ddfee61
浩延 浅川 <asakawa1115@yahoo.co.jp>
2021-04-14 19:47:26 +0900

    2021/04/14

--- WindowsApp1/WindowsApp1/AddSchedule.vb
+++ WindowsApp1/WindowsApp1/AddSchedule.vb
@@ -1,21 +1,8 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class AddSchedule
-
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        Dim selected_startdate = Format(DateTimePicker1.Value, "yyyy/MM/dd")
-
-        Dim selected_enddate = Format(DateTimePicker2.Value, "yyyy/MM/dd")
-
-        Dim selected_starttime = ComboBox1.Text + ":" + ComboBox2.Text
-
-        Dim selected_endtime = ComboBox3.Text + ":" + ComboBox4.Text
-
-        Dim ins_id = Double.Parse(Format(Now(), "hhmmssfff")).ToString()
-
-        Dim event_naiyou = TextBox1.Text
-
+    '画面ロード時
+    Private Sub AddSchedule_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'DB接続
         Dim Builder = New MySqlConnectionStringBuilder()
         ' データベースに接続するために必要な情報をBuilderに与える
         Builder.Server = "localhost"
@@ -28,12 +15,52 @@ Public Class AddSchedule
         Con.ConnectionString = ConStr
         Con.Open()
 
-        Dim SqlStr = "select regist_startdate as 開始日,regist_enddate as 終了日,regist_starttime as 開始時間,regist_endtime as 終了時間, event_name As イベント,insert_id from Schedule"
+        Dim SqlStr = "select regist_startdate as 開始日,regist_enddate as 終了日,regist_starttime as 開始時間,regist_endtime as 終了時間, event_name As イベント,insert_id from Schedule where regist_startdate >= CURDATE() order by regist_startdate asc"
+
+
+        Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
+
+        Dim Ds As New DataSet
+
+        Adapter.Fill(Ds)
+
+        DataGridView1.DataSource = Ds.Tables(0)
+        Con.Close()
+    End Sub
+
+    '登録ボタン押下時
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '開始日付取得
+        Dim selected_startdate = Format(DateTimePicker1.Value, "yyyy/MM/dd")
+        '終了日付取得
+        Dim selected_enddate = Format(DateTimePicker2.Value, "yyyy/MM/dd")
+        '開始時間取得
+        Dim selected_starttime = ComboBox1.Text + ":" + ComboBox2.Text
+        '終了時間取得
+        Dim selected_endtime = ComboBox3.Text + ":" + ComboBox4.Text
+        'Insert_id取得
+        Dim ins_id = Double.Parse(Format(Now(), "hhmmssfff")).ToString()
+        'イベント名取得
+        Dim event_naiyou = TextBox1.Text
+
+        'DB接続
+        Dim Builder = New MySqlConnectionStringBuilder()
+        ' データベースに接続するために必要な情報をBuilderに与える
+        Builder.Server = "localhost"
+        Builder.Port = 3306
+        Builder.UserID = "BKSSCHEDULE"
+        Builder.Password = "bksscd"
+        Builder.Database = "BKSSCHEDULEdb"
+        Dim ConStr = Builder.ToString()
+        Dim Con As New MySqlConnection
+        Con.ConnectionString = ConStr
+        Con.Open()
+
+        Dim SqlStr = "select regist_startdate as 開始日,regist_enddate as 終了日,regist_starttime as 開始時間,regist_endtime as 終了時間, event_name As イベント,insert_id from Schedule where regist_startdate >= CURDATE() order by regist_startdate asc"
         Dim SqlStr2 = "insert into Schedule values('" + ins_id + "','" + selected_endtime + "','" + selected_startdate + "','" + selected_starttime + "','" + selected_endtime + "','" + selected_enddate + "','" + event_naiyou + "')"
 
         Dim adapter2 = New MySqlDataAdapter(SqlStr2, Con)
         Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
-
 
         Dim Ds As New DataSet
         Dim dt As New DataTable
@@ -45,11 +72,19 @@ Public Class AddSchedule
 
     End Sub
 
+    ' カレンダー選択日付の変更時
     Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs) Handles MonthCalendar1.DateChanged
+        ' 選択日付開始日取得
         Dim selected_startdate = MonthCalendar1.SelectionRange.Start.ToString()
+        ' 選択日付終了日取得
         Dim selected_enddate = MonthCalendar1.SelectionRange.End.ToString()
+
+        '登録日付の開始日にセット
         DateTimePicker1.Value = selected_startdate
+        '登録日付の終了日にセット
         DateTimePicker2.Value = selected_enddate
+
+        'DB接続
         Dim Builder = New MySqlConnectionStringBuilder()
         ' データベースに接続するために必要な情報をBuilderに与える
         Builder.Server = "localhost"
@@ -62,38 +97,31 @@ Public Class AddSchedule
         Con.ConnectionString = ConStr
         Con.Open()
         Dim SqlStr = "select regist_startdate as 開始日, regist_enddate As 終了日, regist_starttime As 開始時間, regist_endtime As 終了時間, event_name As イベント,insert_id from Schedule
-        where regist_startdate between '" + selected_startdate + "' and '" + selected_enddate + "'"
+        where regist_startdate between '" + selected_startdate + "' and '" + selected_enddate + "'order by regist_startdate asc"
         Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
         Dim Ds As New DataSet
         Adapter.Fill(Ds)
         DataGridView1.DataSource = Ds.Tables(0)
+
         Con.Close()
 
 
 
     End Sub
 
+    'スケジュール表示画面列選択時
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
+        '「UppSchedule」フォームのインスタンス取得
         Dim uppfm As UppSchedule = New UppSchedule
-
+        '「UppSchedule」フォームの表示
         uppfm.ShowDialog(Me)
 
-        Dim ins_id = DataGridView1.CurrentRow.Cells(6).Value.ToString()
-
-        Dim selectedrowno = DataGridView1.CurrentRow.Index
-
-        Dim dgv As DataGridView = CType(sender, DataGridView)
-
-        If dgv.Columns(e.ColumnIndex).Name = "Column1" Then
-
-            MessageBox.Show(selectedrowno)
-
-        End If
-
+        '画面遷移時のカレンダー選択範囲の取得
         Dim selected_startdate = MonthCalendar1.SelectionRange.Start.ToString()
         Dim selected_enddate = MonthCalendar1.SelectionRange.End.ToString()
 
+        'DB接続
         Dim Builder = New MySqlConnectionStringBuilder()
         ' データベースに接続するために必要な情報をBuilderに与える
         Builder.Server = "localhost"
@@ -106,7 +134,7 @@ Public Class AddSchedule
         Con.ConnectionString = ConStr
         Con.Open()
         Dim SqlStr = "select regist_startdate as 開始日, regist_enddate As 終了日, regist_starttime As 開始時間, regist_endtime As 終了時間, event_name As イベント,insert_id from Schedule
-        where regist_startdate between '" + selected_startdate + "' and '" + selected_enddate + "'"
+        where regist_startdate between '" + selected_startdate + "' and '" + selected_enddate + "'order by regist_startdate asc"
         Dim Adapter = New MySqlDataAdapter(SqlStr, Con)
         Dim Ds As New DataSet
         Adapter.Fill(Ds)
@@ -116,6 +144,7 @@ Public Class AddSchedule
 
     End Sub
 
+    'スケジュール開始日のプロパティ
     Public Property startdateproperty() As String
         Get
             Return DataGridView1.CurrentRow.Cells(1).Value.ToString()
@@ -124,6 +153,7 @@ Public Class AddSchedule
             DataGridView1.CurrentRow.Cells(1).Value = Value
         End Set
     End Property
+    'スケジュール終了日のプロパティ
     Public Property enddateproperty() As String
         Get
             Return DataGridView1.CurrentRow.Cells(2).Value.ToString()
@@ -132,6 +162,7 @@ Public Class AddSchedule
             DataGridView1.CurrentRow.Cells(2).Value = Value
         End Set
     End Property
+    'スケジュール開始時間のプロパティ
     Public Property starttimeproperty() As String
         Get
             Return DataGridView1.CurrentRow.Cells(3).Value.ToString()
@@ -140,6 +171,7 @@ Public Class AddSchedule
             DataGridView1.CurrentRow.Cells(3).Value = Value
         End Set
     End Property
+    'スケジュール終了時間のプロパティ
     Public Property endtimeproperty() As String
 
         Get
@@ -149,6 +181,7 @@ Public Class AddSchedule
             DataGridView1.CurrentRow.Cells(4).Value = Value
         End Set
     End Property
+    'イベント名のプロパティ
     Public Property event_nameproperty() As String
         Get
             Return DataGridView1.CurrentRow.Cells(5).Value.ToString()
@@ -157,6 +190,7 @@ Public Class AddSchedule
             DataGridView1.CurrentRow.Cells(5).Value = Value
         End Set
     End Property
+    'insert_idのプロパティ
     Public Property insert_idproperty() As String
         Get
             Return DataGridView1.CurrentRow.Cells(6).Value.ToString()
@@ -165,7 +199,6 @@ Public Class AddSchedule
             DataGridView1.CurrentRow.Cells(6).Value = Value
         End Set
     End Property
-
 
 
 End Class
--- WindowsApp1/WindowsApp1/UppSchedule.vb
+++ WindowsApp1/WindowsApp1/UppSchedule.vb
@@ -1,7 +1,8 @@
 ﻿Imports MySql.Data.MySqlClient
 Public Class UppSchedule
+    '画面ロード時
     Private Sub UppSchedule_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        '「AddSchedule」画面のテーブル選択列から要素を取得し反映
         DateTimePicker1.Value = AddSchedule.startdateproperty
         DateTimePicker2.Value = AddSchedule.enddateproperty
         ComboBox1.Text = AddSchedule.starttimeproperty.Substring(0, 2)
@@ -11,23 +12,23 @@ Public Class UppSchedule
         TextBox1.Text = AddSchedule.event_nameproperty
         Dim selected_starttime = ComboBox1.Text + ":" + ComboBox2.Text
         Dim selected_endtime = ComboBox3.Text + ":" + ComboBox4.Text
-
     End Sub
-
+    '更新ボタン押下時
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+        '開始日付取得
         Dim selected_startdate = Format(DateTimePicker1.Value, "yyyy/MM/dd")
-
+        '終了日付取得
         Dim selected_enddate = Format(DateTimePicker2.Value, "yyyy/MM/dd")
-
+        '開始時間取得
         Dim selected_starttime = ComboBox1.Text + ":" + ComboBox2.Text
-
+        '終了時間取得
         Dim selected_endtime = ComboBox3.Text + ":" + ComboBox4.Text
-
+        'イベント内容取得
         Dim event_naiyou = TextBox1.Text
-
+        'Insert_id取得
         Dim ins_id = AddSchedule.insert_idproperty
 
+        'DB接続
         Dim Builder = New MySqlConnectionStringBuilder()
         ' データベースに接続するために必要な情報をBuilderに与える
         Builder.Server = "localhost"
@@ -50,14 +51,17 @@ Public Class UppSchedule
 
         Con.Close()
 
+        'フォームを閉じる
         Me.Close()
 
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
 
+        ''「AddSchedule」画面のテーブル選択列のInsert_idプロパティ取得
         Dim ins_id = AddSchedule.insert_idproperty
 
+        'DB接続
         Dim Builder = New MySqlConnectionStringBuilder()
         ' データベースに接続するために必要な情報をBuilderに与える
         Builder.Server = "localhost"
@@ -80,12 +84,14 @@ Public Class UppSchedule
 
         Con.Close()
 
+        'フォームを閉じる
         Me.Close()
 
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
+        'フォームを閉じる
         Me.Close()
 
     End Sub
