4580ed4fba8e03e682bdf7713be363674d28ea52
Nakao <nakao.bks@gmail.com>
2021-04-16 14:56:58 +0900

    コミット

--- WindowsApp1/WindowsApp1/AddSchedule.vb
+++ WindowsApp1/WindowsApp1/AddSchedule.vb
@@ -15,6 +15,7 @@ Public Class AddSchedule
         Con.ConnectionString = ConStr
         Con.Open()
 
+
         Dim SqlStr = "select regist_startdate as 開始日,regist_enddate as 終了日,regist_starttime as 開始時間,regist_endtime as 終了時間, event_name As イベント,insert_id As 登録ID from Schedule where regist_startdate >= CURDATE() order by regist_startdate asc"
 
 
